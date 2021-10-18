672f50313d70402de173062957055fe4aa63d0ed
RMOATMAN <raemarie.oatman@gmail.com>
2021-10-18 19:24:39 -0400

    deploying

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app.js';
+import App from '../src/app.js';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-
 
 // App.js is located in /src
 ReactDOM.render(
@@ -13,7 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
